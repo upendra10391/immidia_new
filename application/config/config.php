bdf4d69eff8fdd31c4f6750d611cec79b51208da
upendra <upendra.etawah@gmail.com>
2017-11-08 22:53:12 +0400

    ch

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/immidia.ae';
-//$config['base_url'] = 'http://localhost/immidia_new/';
+//$config['base_url'] = 'http://localhost/immidia.ae';
+$config['base_url'] = 'http://localhost/immidia_new/';
 
 /*
 |--------------------------------------------------------------------------
